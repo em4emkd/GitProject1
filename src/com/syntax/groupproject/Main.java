adeed9b243985a4d998319b40ae0f3db2a18e3d0
emi <em4emkd@gmail.com>
2022-05-14 12:02:42 -0400

    2nd commit

--- src/com/syntax/groupproject/Main.java
+++ src/com/syntax/groupproject/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println("one");
+        System.out.println("two");
+        System.out.println("three");
+
     }
 }
